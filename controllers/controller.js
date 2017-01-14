883380f13f5dac01fbe34f4e5b68a9190f0988ef
Dhombe <Dhombe>
2017-01-14 19:17:42 +0500

    ufu

---
+++ controllers/controller.js
@@ -0,0 +1,41 @@
+var app= angular.module('mainApp', ['ngRoute']);
+app.config(function($routeProvider){
+  $routeProvider
+  .when('/',{
+    templateUrl: 'page.html'
+  })
+  .when('/info',{
+
+      templateUrl: 'info.html'
+  })
+  .when('/stats',{
+/////////
+/*function Ctrl($scope, $timeout) {
+     $scope.val = false;
+     $timeout(function(){$scope.val = true}, 3000);
+}*/
+/////////
+      templateUrl: 'stats.html'
+  })
+  .when('/medications',{
+
+      templateUrl: 'medications.html'
+  })
+  .when('/enzymes',{
+
+      templateUrl: 'enzymes.html'
+  })
+  .when('/regimen',{
+
+      templateUrl: 'regimen.html'
+  })
+  .when('/foods',{
+
+      templateUrl: 'foods.html'
+  });
+
+/*.otherwise({
+    redirectTo: '/'
+  });*/
+});
+//  console.log('dsffsssfsdfsdf');
